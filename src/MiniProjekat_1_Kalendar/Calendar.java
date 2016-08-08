7cc67cbf741403d16b395c8c9b980a4a98ab3483
Administrator <Administrator@TKPJKA5MDTUK74L>
2016-08-09 00:21:55 +0200

    Ispravak

--- src/MiniProjekat_1_Kalendar/Calendar.java
+++ src/MiniProjekat_1_Kalendar/Calendar.java
@@ -4,16 +4,16 @@ public class Calendar {
 
 	private int year;
 	private int month;
-	
+
 	public Calendar(int year, int month) {
 		this.year = year;
 		this.month = month;
 	}
 
 	/** ispis kalendara */
-	
+
 	public void printMonth(int year, int month) {
-	
+
 		System.out.println("*****************************");
 		printMonthTitle();
 		printMonthBody();
@@ -21,9 +21,8 @@ public class Calendar {
 		System.out.println("*****************************");
 	}
 
-	
 	/** ispis naslova kalendara */
-	
+
 	public void printMonthTitle() {
 		System.out.println("       " + getMonthName(month) + " " + year
 				+ "         ");
@@ -32,58 +31,81 @@ public class Calendar {
 
 	}
 
-	
 	/** nazivi mjeseci */
-	
-	public static String getMonthName(int month){
-			String monthName = "";
-			switch (month) {
-			case 1: monthName = "January"; break;
-			case 2: monthName = "February"; break;
-			case 3: monthName = "Mart"; break;
-			case 4: monthName = "April"; break;
-			case 5: monthName = "May"; break;
-			case 6: monthName = "Jun"; break;
-			case 7: monthName = "July"; break;
-			case 8: monthName = "Avgust"; break;
-			case 9: monthName = "September"; break;
-			case 10: monthName = "Oktober"; break;
-			case 11: monthName = "November"; break;
-			case 12: monthName = "December"; break;
-			}
-			return monthName;  
+
+	public static String getMonthName(int month) {
+		String monthName = "";
+		switch (month) {
+		case 1:
+			monthName = "January";
+			break;
+		case 2:
+			monthName = "February";
+			break;
+		case 3:
+			monthName = "Mart";
+			break;
+		case 4:
+			monthName = "April";
+			break;
+		case 5:
+			monthName = "May";
+			break;
+		case 6:
+			monthName = "Jun";
+			break;
+		case 7:
+			monthName = "July";
+			break;
+		case 8:
+			monthName = "Avgust";
+			break;
+		case 9:
+			monthName = "September";
+			break;
+		case 10:
+			monthName = "Oktober";
+			break;
+		case 11:
+			monthName = "November";
+			break;
+		case 12:
+			monthName = "December";
+			break;
 		}
+		return monthName;
+	}
 
 	/** ispis tijela kalendara */
-	
+
 	public void printMonthBody() {
-		
+
 		// prvi dan u mjesecu
 		int startDay = getStartDay();
 
 		// broj dana u mjesecu
 		int numberOfDaysInMonth = getNumberOfDaysInMonth(year, month);
-		
+
 		// prazna mjesta prije prvog dana u mjesecu
 		int i = 0;
-		for (i = 1; i < startDay; i++){
+		for (i = 1; i < startDay; i++) {
 			System.out.print("    ");
 		}
 		for (i = 1; i <= numberOfDaysInMonth; i++) {
 			System.out.printf("%4d", i);
-		//prelazak u novi red
-			if ((i + startDay-1) % 7 == 0){
+			// prelazak u novi red
+			if ((i + startDay - 1) % 7 == 0) {
 				System.out.println();
+			}
 		}
 	}
-}	
-	
-	/**racunanje prvog dana u mjesecu*/
-	
+
+	/** racunanje prvog dana u mjesecu */
+
 	public int getStartDay() {
 
 		int totalDays = 0;
-		// broj dana nakon ove godine 
+		// broj dana nakon ove godine
 		for (int i = 1800; i < year; i++) {
 			if (isLeapYear(i)) {
 				totalDays += 366;
@@ -94,28 +116,27 @@ public class Calendar {
 		for (int i = 1; i < month; i++) {
 			totalDays += getNumberOfDaysInMonth(i, year);
 		}
-		// ukupan broj dana + 3 dana za prvi dan 1800 (srijeda) + 1 
-		// jer ovaj kalendar pocinje od nedjelje 
+		// ukupan broj dana + 3 dana za prvi dan 1800 (srijeda) + 1
+		// jer ovaj kalendar pocinje od nedjelje
 		return (totalDays + 4) % 7;
 	}
 
-	
 	/** ukupan broj dana */
-	
+
 	public int getTotalNumberOfDays(int year, int month) {
 
 		int total = 0;
 		// ukupan broj dana od 1800 do 1/1/oznacene godine
-		for (int i = 1800; i < year; i++) 
-			if (isLeapYear(i))  //year
+		for (int i = 1800; i < year; i++)
+			if (isLeapYear(i)) // year
 				total = total + 366;
 			else
-				total = total + 365; //*
-		
+				total = total + 365; // *
+
 		// dodaje dane od Jan do izabranog mjeseca u kalendar
-		for (int i = 1; i < month; i++) 
+		for (int i = 1; i < month; i++)
 			total = total + getNumberOfDaysInMonth(year, i);
-	
+
 		return total;
 	}
 
@@ -128,16 +149,18 @@ public class Calendar {
 		if (month == 4 || month == 6 || month == 9 || month == 11)
 			return 30;
 		if (month == 2)
-			return isLeapYear(year) ? 29 : 28; //ako je godina prestupna 28 dana, 29 ako nije
+			return isLeapYear(year) ? 29 : 28; // ako je godina prestupna 28
+												// dana, 29 ako nije
 		return 0; // ako je unos za mjesec netacan (nijedan od ponudjenih)
 	}
 
 	/** provjera da li je godina prestupna */
 	public boolean isLeapYear(int year) {
-		
+
 		if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-			;
-		return true;
+			return true;
+		else
+			return false;
 	}
 
 }
